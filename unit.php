e2739e0edc8da4d3a7aa007327e2457a633f30f2
Adnan <muh.adnansuryaazis@gmail.com>
2022-03-20 19:33:53 +0800

    unit page

---
+++ unit.php
@@ -0,0 +1,131 @@
+<?php 
+session_start();
+include('access/session.php'); 
+include('partials/global.php'); 
+
+ ?>
+
+        
+        
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include('partials/head.php'); ?>
+        
+        <title><?php echo $webname; ?> - Unit</title>        
+    </head>
+    <body>
+        <div class="wrapper">
+            <?php include('partials/topbar_back.php'); ?>
+           
+            <?php include('partials/sidebar.php'); ?>
+           
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Unit</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <button type="button" class="btn btn-success btn-sm float-right"  data-toggle="modal" data-target="#unitModal">Tambah Unit</button>    
+                        </div>          
+                    </div>
+                </div><!-- /.container-fluid -->
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+
+                <!-- Default box -->
+                <div class="card">
+                   
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>                                              
+                                        <th>No. ID</th>
+                                        <th>Model</th>
+                                        <th>Serial Number</th>
+                                        <th>Client</th>
+                                        <th>Tahun</th>
+                                        <th>Foto</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->                    
+                </div>
+                <!-- /.card -->
+
+                </section>
+                <!-- /.content -->
+
+                <div class="modal fade" id="unitModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Tambah Unit Baru</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">                                    
+                                <form action="access/add_unit.php" method="post">                                        
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="no_idTxt">No. ID</label>
+                                        <input class="form-control py-4" id="no_idTxt" type="text" name="no_id"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="modelTxt">Model</label>
+                                        <input class="form-control py-4" id="modelTxt" type="text" name="model"/>
+                                    </div>  
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="serial_numTxt">Serial Number</label>
+                                        <input class="form-control py-4" id="serial_numTxt" type="text" name="serial_num"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="clientTxt">Client</label>
+                                        <input class="form-control py-4" id="clientTxt" type="text" name="client"/>
+                                    </div> 
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="tahunTxt">Tahun</label>
+                                        <input class="form-control py-4" id="tahunTxt" type="number" name="tahun"/>
+                                    </div> 
+                                    <div class="form-group">
+                                        <label  class="small mb-1" for="exampleInputFile">Foto</label>
+                                        <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="exampleInputFile">
+                                            <label class="custom-file-label" for="exampleInputFile">Pilih file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                        </div>
+                                    </div>                                           
+                                    <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">                                                
+                                        <button class="btn btn-primary"  type="submit">Simpan</button>
+                                    </div>
+                                </form>                                    
+                            </div>
+                        
+                        </div>
+                    </div>
+                </div>  
+            </div>
+            <!-- /.content-wrapper -->
+            <?php include('partials/footer.php'); ?>
+        
+           
+        </div>
+        <?php include('partials/scripts.php'); ?>
+    </body>
+</html>
